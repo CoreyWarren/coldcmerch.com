cf6536e363421beeb9ddcd893703079e087edbf8
Corey <coreywarrenii@hotmail.com>
2023-05-13 20:34:33 -0700

    checkout view laid out. May need race condition considerations.

--- django/coldcmerch/shop/views/checkoutViews.py
+++ django/coldcmerch/shop/views/checkoutViews.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
+import json
 
 # CHECKOUT VIEWS
 
@@ -20,9 +22,7 @@ from rest_framework.permissions import IsAuthenticated
 # clicks the "Checkout" button.
 
 class CheckoutValidateStock(APIView):
-    permission_classes = [IsAuthenticated]
 
-    def post(self, request):
         # Input:
 
         # request.user
@@ -36,46 +36,15 @@ class CheckoutValidateStock(APIView):
         #    "quantity": 1
         # }
 
-
-        cart_items = request.cart_items
-        this_user = request.user
-
-
         # Process:
 
         # For each item in request.cart_items, check if the product is in stock compared to the amount that the user is ordering. 
 
-        item_stock_dict = {}
-        out_of_stock_items = []
         # {product_size_id: X, stock_left: Y}
 
         # assemble the quantity for each type of product size in the cart
         # We will need to do some calculations after this for statement,
         # In order to check if the requested amount is available in stock.
-        for item in cart_items:
-            some_product = Product.objects.get(id=item['product'])
-            item_size = item['size']
-            some_product_size = ProductSize.objects.get(product=some_product, size=item_size)
-
-            requested_amount = item['quantity']
-
-            if some_product_size.id not in item_stock_dict:
-                item_stock_dict[some_product_size.id] \
-                    = some_product_size.available_amount - requested_amount
-                
-                if item_stock_dict[some_product_size.id] < 0:
-                    out_of_stock_items.append({
-                        "product": item['product'],
-                        "size": item['size'],
-                        "adjusted_total": item['adjusted_total'],
-                        "quantity": item['quantity'],
-                        "available_quantity": some_product_size.available_amount
-                    })
-                    success = False
-
-            else:
-                item_stock_dict[some_product_size.id] -= requested_amount
-
 
 
 
@@ -86,63 +55,122 @@ class CheckoutValidateStock(APIView):
         ###
         # 1. In stock or not? (True/False)
         ###
-
-
-
-
         ###
         # 2. If not (1/2), then which product and size is out of stock?
         ###
-
-
-
         ###
         # 3. If not (2/2), then how many are in stock?
         ###
 
+    permission_classes = [IsAuthenticated]
+
+
+    def post(self, request):
+        # Transaction.atomic() is used to ensure that all database operations are completed successfully, or none of them are.
+        # This is useful because we are making multiple database operations in this view.
+        # Additionally, if MULTIPLE USERS are trying to checkout at the same time, we don't want to run into any issues with the database.
+        # transaction.atomic() will roll back all database operations if any of them fail.
+        with transaction.atomic():
+            cart_items = request.data['cart_items']
+            this_user = request.user
+
+            # item_stock_dict is used to keep track of the remaining stock for each product size.
+            item_stock_dict = {}
+            
+            # total_requested_dict is used to keep track of the total quantity requested for each product size.
+            total_requested_dict = {}
+            
+            # out_of_stock_items is a list that will hold dictionaries of out of stock items,
+            # where each dictionary contains the product, size, adjusted total, quantity requested, and available quantity.
+            out_of_stock_items = []
+            
+            # success is a boolean flag that will remain True if all items are in stock, and False if any item is out of stock.
+            success = True
 
 
 
+            # Loop over each item in the cart
+            for item in cart_items:
+                # Fetch the Product instance using the product id from the cart item
+                some_product = Product.objects.get(id=item['product'])
 
+                # Get the size of the product from the cart item
+                item_size = item['size']
 
+                # Fetch the ProductSize instance using the fetched product and size
+                some_product_size = ProductSize.objects.get(product=some_product, size=item_size)
 
+                # Get the quantity of the product requested from the cart item
+                requested_amount = item['quantity']
 
-        # Output:
+                # Check if the product size id is already present in item_stock_dict
+                if some_product_size.id not in item_stock_dict:
+                    # If not, then add it to the item_stock_dict with the available amount from the product size
+                    item_stock_dict[some_product_size.id] = some_product_size.available_amount
 
-        # {
-        # "success": true,
-        # "message": "All items are in stock."
-        # }
+                    # Also add it to the total_requested_dict with the requested amount
+                    total_requested_dict[some_product_size.id] = requested_amount
+                else:
+                    # If the product size id is already present, then increment the total requested amount in total_requested_dict
+                    total_requested_dict[some_product_size.id] += requested_amount
 
-        # OR
+                # Check if the available stock for the product size is greater than or equal to the requested amount
+                if item_stock_dict[some_product_size.id] - requested_amount >= 0:
+                    # If yes, then subtract the requested amount from the available stock in item_stock_dict
+                    item_stock_dict[some_product_size.id] -= requested_amount
+                else:
+                    # If not, then append the cart item to the out_of_stock_items list,
+                    # with additional fields for total requested quantity and available quantity,
+                    # and set the success flag to False
+                    out_of_stock_items.append({
+                        "product": item['product'],
+                        "size": item['size'],
+                        "adjusted_total": item['adjusted_total'],
+                        "quantity": total_requested_dict[some_product_size.id],
+                        "available_quantity": item_stock_dict[some_product_size.id]
+                    })
+                    success = False
 
-        # {
-        #     "success": false,
-        #     "message": "Some items not in stock.",
-        #     "out_of_stock_items": [
-        #         {
-        #             "product": 1,
-        #             "size": "S",
-        #             "adjusted_total": 20,
-        #             "quantity": 3
-        #             "available_quantity": 2
-        #         },
-        #         {
-        #             "product": 2,
-        #             "size": "M",
-        #             "adjusted_total": 20,
-        #             "quantity": 3
-        #             "available_quantity": 2
-        #         }
-        #     ]
-        # }
+            # Output:
 
-        ########################################
-        success = True
-        response = {
-            "success": success,
-        }
-        if (success):
-            return Response(response, status=status.HTTP_200_OK)
-        else:
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            # {
+            # "success": true,
+            # "message": "All items are in stock."
+            # }
+
+            # OR
+
+            # {
+            #     "success": false,
+            #     "message": "Some items not in stock.",
+            #     "out_of_stock_items": [
+            #         {
+            #             "product": 1,
+            #             "size": "S",
+            #             "adjusted_total": 20,
+            #             "quantity": 3
+            #             "available_quantity": 2
+            #         },
+            #         {
+            #             "product": 2,
+            #             "size": "M",
+            #             "adjusted_total": 20,
+            #             "quantity": 3
+            #             "available_quantity": 2
+            #         }
+            #     ]
+            # }
+
+            ########################################
+
+            response = {"success": success}
+            if success:
+                response["message"] = "All items are in stock."
+            else:
+                response["message"] = "Some items are not in stock."
+                response["out_of_stock_items"] = out_of_stock_items
+
+            if success:
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                return Response(response, status=status.HTTP_409_CONFLICT)
