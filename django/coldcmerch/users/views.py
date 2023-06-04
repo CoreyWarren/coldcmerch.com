77c7c20a7ddfaf3d43bb032f8e213ac4088cb893
Corey <coreywarrenii@hotmail.com>
2023-06-04 15:22:08 -0700

    Fixed import dependencies on Django IsAuthenticated

--- django/coldcmerch/users/views.py
+++ django/coldcmerch/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import permissions, status
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import UserCreateSerializer, UserSerializer
 from shop.models import Cart
